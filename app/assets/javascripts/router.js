dabeee8ab6f81dc7f1ba6ac5de9b8612633ca95d
Abhirama sundram <abhiramasundram2007@gmail.com>
2014-02-06 21:59:11 +0530

    kick start ;)

---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+EmberStart.Router.map(function() {
+  // this.resource('posts');
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberStart::Application.config.secret_key_base = 'f7c91d3434cc3420b4aac3382f6945221842cdc9d586a795a82d6bdfa156bf6d278c6e5daa49fadb3447d8c5df3e9e38d0dceb2e0ef40214db026334389de7db'
